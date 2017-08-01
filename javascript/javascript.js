17f74abf48885dc765d0d75ace9257179ac301fd
Jesse Smith <JesseSmith3rd@outlook.com>
2017-07-31 22:59:12 -0400

    Updated a card to HTML and Add array to Javascript

---
+++ javascript/javascript.js
@@ -0,0 +1,24 @@
+var newArray = [{
+  name: Roshonda Smith,
+  bankAccount: 50000000.00
+},
+{
+  name: Laila Smith,
+  bankAccount: 1000000.00
+}{
+  name: Jesse Smith,
+  bankAccount: 70000000.00
+}{
+  name: FeFe,
+  bankAccount: 5000000.00
+}{
+  name: Tyler,
+  bankAccount: 20000000.00
+}{
+  name: Zay,
+  bankAccount: 2000000.00
+}];
+
+for (var i = 0; i < newArray.length; i++) {
+  var newCard = $(div class="card");
+}
